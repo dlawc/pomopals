b6e9b4f104ebedbba2d37e44786f25a1ba0009dc
edwinhrj <122248645+edwinhrj@users.noreply.github.com>
2024-04-18 15:26:04 +0800

    updated host home page

--- pomopals/src/components/SessionCode.vue
+++ pomopals/src/components/SessionCode.vue
@@ -42,7 +42,6 @@ export default {
       this.sessionCode =
         Date.now().toString(36) + Math.random().toString(36).substring(2);
       this.$emit("cancelDurationEvent");
-      this.$emit("updateSessionCode", this.sessionCode);
       let currentUser = firebaseAuth.currentUser;
       let username = currentUser.displayName; // username as primary key
       let userRef = firestore.collection("groupSession").doc(this.sessionCode);
@@ -53,8 +52,18 @@ export default {
         restDuration: 0,
         timerDuration: 0,
       };
-      userRef.set(data);
-      this.$router.push("/host");
+      userRef
+        .set(data)
+        .then(() => {
+          // Only navigate once the Firestore set is successful
+          this.$router.push({
+            path: "/host",
+            query: { sessionCode: this.sessionCode },
+          });
+        })
+        .catch((error) => {
+          console.error("Error setting session data:", error);
+        });
     },
     // adds user to the members array of the group session document
     enterCode() {
--- pomopals/src/views/HostHomePage.vue
+++ pomopals/src/views/HostHomePage.vue
@@ -1,43 +1,58 @@
+<template>
+  <div>
+    <span id="sessionCode">Session Code: {{ sessionCode }}</span>
+  </div>
+  <div class="home">
+    <NavBar />
+    <XpBar />
+    <Timer ref="timerRef" />
+  </div>
+</template>
+
 <script>
 import Timer from "/src/components/Timer.vue";
 import XpBar from "/src/components/XpBar.vue";
 import NavBar from "@/components/NavBar.vue";
-import SessionCode from "@/components/SessionCode.vue";
 
 export default {
   name: "HostHomePage",
-  components: { XpBar, Timer, NavBar, SessionCode },
+  components: {
+    Timer,
+    XpBar,
+    NavBar,
+  },
   data() {
     return {
       sessionCode: "",
     };
   },
-  methods: {
-    handleDisplaySessionCode(code) {
-      this.sessionCode = code;
-    },
+  created() {
+    // Set the sessionCode based on the router query parameter
+    this.sessionCode = this.$route.query.sessionCode;
+    console.log("Received session code:", this.sessionCode);
   },
 };
 </script>
 
-<template>
-  <div class="home">
-    <div><NavBar /></div>
-    <div>
-      <SessionCode
-        v-if="false"
-        @displaySessionCode="handleDisplaySessionCode"
-      />
-    </div>
-    <div><XpBar /></div>
-    <div><Timer ref="timerRef" /></div>
-    <p1 id="groupStudy">
-      {{ sessionCode }}
-    </p1>
-  </div>
-</template>
-
 <style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+#sessionCode {
+  left: auto;
+  right: auto;
+  top: 0px;
+  position: absolute;
+  margin-top: 20px; /* Add more space above the session code display */
+  font-size: 24px;
+  color: white;
+  font-weight: 700;
+  font: "lucida sans";
+}
 #friendsButton,
 #leaderboardButton {
   position: absolute;
