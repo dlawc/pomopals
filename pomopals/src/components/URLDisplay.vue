7d38bbe4b913c27323dd54bbbde0f1d426ffd960
ariewe <ewearissa@gmail.com>
2024-04-22 01:03:55 +0800

    Updated URLDisplay.vue, URLBlockerPage.vue and NavBar.vue
    fixed friends leaderboard nav bar, url delete button and input button

--- pomopals/src/components/URLDisplay.vue
+++ pomopals/src/components/URLDisplay.vue
@@ -1,103 +1,145 @@
-<template>
-    <h1>My Blocklist</h1>
-    <table id = urls>
-    </table>
-</template>
 <style>
     h1{
         text-align: center;
         color: white;
         font-size: 60px;
     }
+    h2{
+        text-align: center;
+        color: white;
+    }
     .bwt{
         background-color: red;
         color: white; 
         border: none;
     }
+    .bwt:hover {
+        background-color: brown;
+    }
+    #form {
+        text-align: center;
+        display: flex;
+    }
+    input {
+        padding: 5px 10px; 
+        width: 100%; 
+    }
 </style>
+<template>
+    <h2>Block sites <u>distracting</u> you:</h2>
+    <div id = form> 
+        <input type = "text" v-model = "url" required = "true" 
+            placeholder = "Enter URL of website here" @keydown.enter="saveurl()"> 
+        <!--<button id = "save" type = "button" @click="saveurl()">Save</button>-->
+    </div>
+    <h1>My Blocklist</h1>
+    <table id = urls>
+    </table>
+</template>
 <script>
 import firebase from "@/firebase";
 export default {
-    async mounted() {
-        this.display();  
+    data() {
+        return {
+            url: ''
+        }
     }, 
-    Name: "Blocklist", 
+    async mounted() {
+        this.display();
+    },
     methods: {
-        async display() { 
-            //let index = 1; 
-            const db = firebase.firestore(); 
+        async display() {
+            const db = firebase.firestore();
             const username = firebase.auth().currentUser.displayName;
             const userRef = db.collection("users").doc(username);
-            userRef.get().then((doc) => {
+
+            try {
+                const doc = await userRef.get();
                 if (doc.exists) {
-                    // Document exists, retrieve the urlblocklist array
                     const urlBlocklist = doc.data().urlblocklist;
+                    const table = document.getElementById("urls");
 
-                    // Reference to the table
-                    const table = document.getElementById('urls');
+                    // Clear existing rows
+                    table.innerHTML = "";
 
-                    // Loop through the urlblocklist array and add each URL to the table
+                    // Iterate over the URL blocklist and create table rows
                     urlBlocklist.forEach((url) => {
-                        // Create a new row
                         const newRow = table.insertRow();
-
-                        // Create a cell for the URL and add it to the row
                         const urlCell = newRow.insertCell();
-                        urlCell.classname = "urlcell"
+                        urlCell.className = "urlcell";
                         urlCell.textContent = url;
                         urlCell.style.color = "white";
                         urlCell.style.fontWeight = "bold";
 
-                        let deleteButton = document.createElement("button") 
-                        deleteButton.id = String(url)
-                        deleteButton.className = "bwt"
-                        deleteButton.innerHTML = "Delete"
-                        urlCell.appendChild(deleteButton)
-                        deleteButton.onclick = function() {
-                            deleteUrl(url)
-                        }
+                        const deleteButton = document.createElement("button");
+                        const deleteCell = newRow.insertCell(); 
+                        deleteButton.className = "bwt";
+                        deleteButton.textContent = "Delete";
+                        deleteButton.onclick = () => {
+                            this.deleteUrl(url);
+                        };
+                        deleteCell.appendChild(deleteButton);
                     });
                 } else {
-                // Document does not exist
-                console.log("No such document!");
+                    console.log("No such URLs!");
                 }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
-            });
-        }, 
+            } catch (error) {
+                console.error("Error getting user:", error);
+            }
+        },
         async deleteUrl(url) {
-            const db = firebase.firestore(); 
+            const db = firebase.firestore();
             const username = firebase.auth().currentUser.displayName;
-            const userRef = db.collection("users").doc(username); 
-            userRef.get().then((doc) => {
+            const userRef = db.collection("users").doc(username);
+
+            try {
+                const doc = await userRef.get();
                 if (doc.exists) {
-                    // Document exists, retrieve the urlblocklist array
-                    const urlBlocklist = doc.data().urlblocklist;
-
-                    // Find the index of the URL to delete in the urlblocklist array
-                    const indexToDelete = urlBlocklist.indexOf(urlToDelete);
-
+                    let urlBlocklist = doc.data().urlblocklist;
+                    const indexToDelete = urlBlocklist.indexOf(url);
                     if (indexToDelete !== -1) {
-                        // Remove the URL from the urlblocklist array
                         urlBlocklist.splice(indexToDelete, 1);
+                        await userRef.update({ urlblocklist: urlBlocklist });
+                        console.log("URL deleted successfully!");
 
-                        // Update the document with the modified urlblocklist array
-                        userRef.update({ urlblocklist: urlBlocklist }).then(() => {
-                            console.log("URL deleted successfully!");
-                        }).catch((error) => {
-                            console.error("Error updating document:", error);
-                        });
+                        
                     } else {
                         console.log("URL not found in the blocklist.");
                     }
                 } else {
-                    // Document does not exist
-                 console.log("No such document!");
+                    console.log("No such user!");
                 }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
+            } catch (error) {
+                console.error("Error updating URLBlockList:", error);
+            }
+            await this.display(); 
+        }, 
+        async saveurl() {
+            const db = firebase.firestore(); 
+            const username = firebase.auth().currentUser.displayName;
+            const userRef = db.collection("users").doc(username);
+            userRef.get().then(doc => {
+                if (doc.exists) {
+                    const data = doc.data();
+                    const existingUrlBlocklist = data.urlblocklist || [];
+                    // Append the new URL to the existing list
+                    const updatedUrlBlocklist = [...existingUrlBlocklist, this.url.trim()];
+                    // Update the user document with the updated urlblocklist
+                    userRef.update({ urlblocklist: updatedUrlBlocklist }).then(async () => {
+                        console.log("URL added successfully!");
+                        // Clear the input field after successful save
+                        this.url = '';
+                        await this.display()
+                    }).catch(error => {
+                        console.error("Error updating user document:", error);
+                    });
+                } else {
+                    console.log("No such user!");
+                }
+            }).catch(error => {
+                console.log("Error getting user:", error);
             });
         }
     }
-}
+};
 </script>
--- pomopals/src/views/URLBlockerPage.vue
+++ pomopals/src/views/URLBlockerPage.vue
@@ -5,18 +5,11 @@
     <div class = navbar>
         <NavBar/>
     </div>
-    
     <div class = center>
-        <h2>Block sites <u>distracting</u> you:</h2>
-        <div id = form> 
-            <input type = "text" v-model = "url" required = "true" placeholder = "Enter URL of website here"> 
-            <button id = "save" type = "button" @click="saveurl()">Save</button>
-        </div>
         <URLDisplay/>
     </div>
 </template>
 <script>
-import firebase from "@/firebase";
 import NavBar from "@/components/NavBar.vue"
 import SignOutButton from "@/components/SignOutButton.vue"
 import URLDisplay from "@/components/URLDisplay.vue"
@@ -28,47 +21,5 @@ export default {
         SignOutButton, 
         URLDisplay,
     }, 
-    data() {
-        return {
-            url: ''
-        }
-    }, 
-    methods: {
-        saveurl() {
-            const db = firebase.firestore(); 
-            const username = firebase.auth().currentUser.displayName;
-            const userRef = db.collection("users").doc(username);
-            userRef.get().then(doc => {
-                if (doc.exists) {
-                    const data = doc.data();
-                    const existingUrlBlocklist = data.urlblocklist || [];
-                    // Append the new URL to the existing list
-                    const updatedUrlBlocklist = [...existingUrlBlocklist, this.url.trim()];
-                    // Update the user document with the updated urlblocklist
-                    userRef.update({ urlblocklist: updatedUrlBlocklist });
-                } else {
-                    console.log("No such user!");
-                }
-            }).catch(error => {
-                console.log("Error getting user:", error);
-            });
-        }
-    }
 }
-
-
 </script>
-<style>
-    h2{
-        text-align: center;
-        color: white;
-    }
-    #form {
-        text-align: center;
-        display: flex;
-    }
-    input {
-        padding: 5px 10px; 
-        width: 100%; 
-    }
-</style>
