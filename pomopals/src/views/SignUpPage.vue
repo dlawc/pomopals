0882b1732caf0696c46b9eb26e74ba5aa2c15513
Isabel <isabelleewy@gmail.com>
2024-04-25 00:01:33 +0800

    Updated background

--- pomopals/src/views/SignUpPage.vue
+++ pomopals/src/views/SignUpPage.vue
@@ -195,22 +195,6 @@ export default {
 </script>
 
 <style scoped>
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url("/bg.jpeg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-}
 
 .login-container {
   flex-direction: column;
--- pomopals/src/views/VerifyEmailPage.vue
+++ pomopals/src/views/VerifyEmailPage.vue
@@ -68,20 +68,6 @@
   
 
 <style scoped>
-.overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  background:linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url("/bg.jpeg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
 .login-container {
   background: white;
