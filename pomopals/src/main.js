630f9232bdff44384ae17054a5ef18f9a4018674
Isabel <isabelleewy@gmail.com>
2024-04-25 21:14:56 +0800

    Updated loading of website

--- pomopals/src/main.js
+++ pomopals/src/main.js
@@ -1,18 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // if using Vue Router
-import firebase from './firebase' // the path to the firebase module
+import router from './router' 
+import firebase from './firebase' 
 import 'firebaseui/dist/firebaseui.css'
-import './assets/global.css'; // Adjust the path to your global styles
+import './assets/global.css'; 
 
 
 const app = createApp(App)
 app.use(router)
-
-// Optionally: If you're using Firebase's Firestore or Auth in your components, you might want to attach them to the Vue instance for easier access:
 app.config.globalProperties.$firebase = firebase
-// This is not strictly necessary, but can make accessing Firebase easier within your components, using `this.$firebase`
-
 app.mount('#app')
 
 
